31c9fc128408442a7596f08c5f17a483aae8a5f2
Muhammad iqran <iqrannwl@gmail.com>
2023-02-08 13:57:42 +0500

    comments

--- 1.Comments/comments.rb
+++ 1.Comments/comments.rb
@@ -8,5 +8,6 @@ puts "In Above mention line that is comment syntex in ruby \n"
 This is a multi line comment
 
 =end
-
 puts "Above mention line are represent the multi line comment in Ruby!\n"
+
+puts "Comments are useful in any language"
